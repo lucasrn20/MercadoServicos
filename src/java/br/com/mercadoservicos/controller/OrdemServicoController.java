94f9565f28c9d977539d88c416dbc6d643a82d32
lucasrn20 <lucasnunes.dabarra@hotmail.com>
2020-05-19 22:34:26 -0300

    Mudanças em new OrdemServico

--- src/java/br/com/mercadoservicos/controller/OrdemServicoController.java
+++ src/java/br/com/mercadoservicos/controller/OrdemServicoController.java
@@ -52,8 +52,7 @@ public class OrdemServicoController implements Serializable{
     }
     
     public String salvar(){
-        ordemServico.setItensOs(itensOrdemServico);
-        if(ordemServicoService.inserir(ordemServico)){
+        if(ordemServicoService.inserir(ordemServico, itensOrdemServico)){
             UtilMensagens.mensagemSucesso("Sucesso", "Ordem de serviço salva com sucesso!");
             this.listar();
             return "list.xhtml?faces-redirect=true"; 
@@ -80,7 +79,7 @@ public class OrdemServicoController implements Serializable{
     public void removeServico(ItensOrdemServico itemOrdemServico ){
         itensOrdemServico.remove(itemOrdemServico);
     }
-
+    
     public void calculaTotal(){
         itemOrdemServico.setPreco(itemOrdemServico.getQuantidade() * itemOrdemServico.getServico().getPreco());
     }
